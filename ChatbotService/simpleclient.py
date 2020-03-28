64d32401958f16079a2a5f86061cd32125a230fb
marius <mfc@marius-cristian.com>
2020-03-28 14:18:50 +0100

    small sanitization

--- ChatbotService/simpleclient.py
+++ ChatbotService/simpleclient.py
@@ -6,13 +6,13 @@ async def work():
     # blocking call
     value = input("Chat here:\n")
     # did not implement http post due to lax project requirements
-    # This service should receive a message and return a response taken from a request to the data
-    # service
+    # more details in Report.txt
     async with aiohttp.ClientSession() as session:
         async with session.get('http://localhost:8000/random') as resp:
             data = await resp.json()
             # TODO figure a better way to make this deserialization
             print(data["data"][0]["attributes"]["content"])
+    print ("ok")
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()

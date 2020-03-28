f0d25277826c2cb86e460890b7cd36de31ecdf75
marius <mfc@marius-cristian.com>
2020-03-28 15:04:20 +0100

    Over the deadline quick refactor

--- DataService/chat/urls.py
+++ DataService/chat/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import include, url
 from rest_framework import routers
 
-from .views import ChatMessageViewset
-from .views import RandomChatMessage
+from .views import StandardViewSet
+from .views import RandomViewSet
 
 api_router = routers.SimpleRouter()
-api_router.register(r'chatmessages', ChatMessageViewset, basename='chatmessage')
-api_router.register(r'random', RandomChatMessage, basename='randommessage')
+api_router.register(r'chatmessages', StandardViewSet, basename='chatmessage')
+api_router.register(r'random', RandomViewSet, basename='randommessage')
 
 urlpatterns = [
     url(r'', include(api_router.urls))
--- DataService/chat/views.py
+++ DataService/chat/views.py
@@ -8,33 +8,16 @@ from .serializer import ChatMessageSerializer
 from rest_framework.decorators import action
 
 class BaseViewSet(viewsets.ModelViewSet):
+    serializer_class = ChatMessageSerializer
+    model = ChatMessage
+
+
+
+class StandardViewSet(BaseViewSet):
     def get_queryset(self):
         return self.model.objects.all()
 
 
-
-# must refactor to pass parameter for example: 
-class ChatMessageViewset(BaseViewSet):
-    serializer_class = ChatMessageSerializer
-    model = ChatMessage
-
-    #@action(detail=True, methods=['get'])
-    #def retrieve(self, request):
-    #    queryset = self.model.objects.filter().random()
-    #    user = get_object_or_404(queryset)
-    #    serializer = ChatMessageSerializer(user)
-    #    return Response(serializer.data)
-
-
-
-# must refactor this, its duplicate code
-# I don know if its optimal (do not know what is happening
-# in django_random_queryset RandomManager under the hood)
-class RandomViewSet(viewsets.ModelViewSet):
+class RandomViewSet(BaseViewSet):
     def get_queryset(self):
         return self.model.objects.filter().random()
-
-
-class RandomChatMessage(RandomViewSet):
-    serializer_class = ChatMessageSerializer
-    model = ChatMessage
